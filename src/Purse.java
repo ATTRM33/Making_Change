271b9178d68758a76e116d6ae2e13535ff136469
ATTRM33 <ATTRM33@gmail.com>
2024-09-02 14:45:16 -0500

    added getValue() function to Purse Class

--- src/Purse.java
+++ src/Purse.java
@@ -8,21 +8,27 @@ public class Purse {
         this.cash = new HashMap<>();
     }
 
-    public void add(MoneyType type, int num){
+    public void add(MoneyType type, int num) {
         cash.put(type, cash.getOrDefault(type, 0) + num);
     }
 
-    public double remove(MoneyType type, int num){
-        //current count of money type in the purse
+    public double remove(MoneyType type, int num) {
         int count = cash.getOrDefault(type, 0); //current count of money type in the purse
 
         if (count < num) {
-            count = num;
+            count = num; // if value entered is larger than the amt in the purse then count is equal to number
         }
         if (num > 0) {
             cash.put(type, count - num);
         }
         return num * type.amt();
+    }
+
+    public double getValue() {
+
+        //returns set of all entries, converts them to stream of doubles, takes the value and multiplies it by
+        //the number of entries and returns the sum of all values
+        return cash.entrySet().stream().mapToDouble(entry -> entry.getKey().amt() * entry.getValue()).sum();
     }
 }
 
