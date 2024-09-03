a9f7c39e7d4b67062b5c3883bd749a45379e159d
ATTRM33 <ATTRM33@gmail.com>
2024-09-03 15:05:27 -0500

    typo on fifty_note.png

--- src/Register.java
+++ src/Register.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Register {
     private static final List<MoneyType> MONEY = Arrays.asList(
             new MoneyType("Hundred Dollar", 100.0, "cash","hundred_note.png"),
-            new MoneyType("Fifty Dollar", 50.0, "cash", "fifty_not.png"),
+            new MoneyType("Fifty Dollar", 50.0, "cash", "fifty_note.png"),
             new MoneyType("Twenty Dollar", 20.0,"cash", "twenty_note.png"),
             new MoneyType("Ten Dollar", 10.0, "cash", "ten_note.png"),
             new MoneyType("Five Dollar", 5.0, "cash", "five_note.png"),
